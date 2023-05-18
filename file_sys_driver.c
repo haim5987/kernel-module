16fd5e642737bfce3b81abd78d1293cf3b74f20e
haim5987 <haim5987@gmail.com>
2023-05-18 12:11:57 +0300

    modify file sys

--- file_sys_driver.c
+++ file_sys_driver.c
@@ -9,69 +9,30 @@ MODULE_AUTHOR("Haim Kasel");
 MODULE_DESCRIPTION("Read-only Hello World file system driver");
 
 
-#define HELLO_FILE_CONTENT "Hello world!\n"
-#define HELLO_FILE_SIZE (sizeof(HELLO_FILE_CONTENT) - 1)
+static struct file_system_type read_only_fs_type = {
+    .name = "hello",
+    .kill_sb = kill_litter_super,
+};
 
-static struct file_operations hello_fops;
-
-static struct inode *hello_inode(struct super_block *sb, int mode)
-{
-    struct inode *inode = new_inode(sb);
-    if (inode) {
-        inode->i_ino = get_next_ino();
-        inode->i_mode = mode;
-        inode->i_op = &hello_fops;
-        inode->i_fop = &hello_fops;
-        inode->i_size = HELLO_FILE_SIZE;
-    }
-    return inode;
-}
-
-static ssize_t hello_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
-{
-    return simple_read_from_buffer(buf, len, off, HELLO_FILE_CONTENT, HELLO_FILE_SIZE);
-}
-
-static int __init hello_init(void)
-{
-    struct dentry *dentry;
-    struct super_block *sb;
-    struct inode *inode;
-
-    sb = sget_userns(NULL, &hello_fops, NULL, 0, MS_RDONLY);
-    if (IS_ERR(sb)) {
-        pr_err("Failed to create superblock\n");
-        return PTR_ERR(sb);
+static int __init read_only_fs_init(void) {
+    int ret = register_filesystem(&read_only_fs_type);
+    if (ret != 0) {
+        pr_err("Failed to register read only file system\n");
+        return ret;
     }
 
-    sb->s_blocksize = PAGE_SIZE;
-    sb->s_blocksize_bits = PAGE_SHIFT;
-
-    dentry = d_make_root(inode);
-    if (!dentry) {
-        pr_err("Failed to create root dentry\n");
-        return -ENOMEM;
-    }
-
-    sb->s_root = dentry;
-    inode = hello_inode(sb, S_IFREG | S_IRUGO);
-    if (!inode) {
-        pr_err("Failed to create inode\n");
-        return -ENOMEM;
-    }
-    d_add(dentry, inode);
-
+    pr_info("read only file system module loaded\n");
     return 0;
 }
 
-static void __exit hello_exit(void)
-{
-    return;
+
+static void __exit read_only_fs_exit(void) {
+    int ret = unregister_filesystem(&read_only_fs_type);
+    if (ret != 0)
+        pr_err("Failed to unregister read only file system\n");
+
+    pr_info("read only file system module unloaded\n");
 }
 
-static struct file_operations hello_fops = {
-    .read = hello_read,
-};
-
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(read_only_fs_init);
+module_exit(read_only_fs_exit);
