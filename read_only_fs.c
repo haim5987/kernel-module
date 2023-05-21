985675ecc797b1d44f84e6c2691d610f24821b86
haim5987 <haim5987@gmail.com>
2023-05-21 15:15:17 +0300

    modify file sys

--- read_only_fs.c
+++ read_only_fs.c
@@ -4,7 +4,6 @@
 #include <linux/init.h>
 #include <linux/slab.h>
 
-
 #define FILE_SYSTEM_MAGIC 0x12345678
 
 static const struct address_space_operations custom_fs_aops;
@@ -14,6 +13,7 @@ static const struct dentry_operations custom_fs_dentry_operations;
 // Structure to hold custom file system data
 struct custom_fs_data {
     struct super_block *sb;
+    unsigned long long fib_num;  // Next number of Fibonacci sequence
 };
 
 // Custom file system file operations
@@ -29,15 +29,24 @@ static const struct super_operations custom_fs_super_operations = {
 
 // Custom file system inode operations
 static const struct inode_operations custom_fs_inode_operations = {
-    .setattr	= simple_setattr,
-	.getattr	= simple_getattr,
+    .setattr = simple_setattr,
+    .getattr = simple_getattr,
 };
 
+// Fibonacci sequence calculation
+static unsigned long long fibonacci(unsigned long long n)
+{
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 // Initialize custom file system superblock
 static int custom_fs_fill_super(struct super_block *sb, void *data, int silent)
 {
     struct inode *root_inode;
+    struct custom_fs_data *fs_data;
 
     sb->s_magic = FILE_SYSTEM_MAGIC;
     sb->s_op = &custom_fs_super_operations;
@@ -50,11 +59,21 @@ static int custom_fs_fill_super(struct super_block *sb, void *data, int silent)
     root_inode->i_sb = sb;
     root_inode->i_op = &custom_fs_inode_operations;
     root_inode->i_fop = &custom_fs_file_operations;
-    root_inode->i_atime = root_inode->i_mtime = root_inode->i_ctime =  current_time(root_inode);;
+    root_inode->i_atime = root_inode->i_mtime = root_inode->i_ctime = current_time(root_inode);
 
     sb->s_root = d_make_root(root_inode);
     if (!sb->s_root)
         return -ENOMEM;
+
+    // Allocate memory for custom file system data
+    fs_data = kzalloc(sizeof(struct custom_fs_data), GFP_KERNEL);
+    if (!fs_data)
+        return -ENOMEM;
+
+    fs_data->sb = sb;
+    fs_data->fib_num = 1;  // Initialize with the first number of the Fibonacci sequence
+
+    sb->s_fs_info = fs_data;
 
     return 0;
 }
@@ -80,9 +99,32 @@ static struct inode *custom_fs_get_inode(struct super_block *sb, int mode)
     return inode;
 }
 
+// Custom file system file read operation
+static ssize_t custom_fs_file_read(struct file *filp, char __user *buf, size_t len, loff_t *ppos)
+{
+    struct custom_fs_data *fs_data = filp->f_path.dentry->d_sb->s_fs_info;
+    char fib_num_str[64];
+    ssize_t ret;
+
+    // Calculate the next Fibonacci number
+    fs_data->fib_num = fibonacci(fs_data->fib_num);
+
+    // Convert the Fibonacci number to a string
+    snprintf(fib_num_str, sizeof(fib_num_str), "%llu\n", fs_data->fib_num);
+
+    // Copy the Fibonacci number string to the user buffer
+    ret = simple_read_from_buffer(buf, len, ppos, fib_num_str, strlen(fib_num_str));
+
+    return ret;
+}
+
+// Custom file system file operations
+static const struct file_operations custom_fs_file_operations = {
+    .read = custom_fs_file_read,
+};
+
 // Mount the custom file system
-static struct dentry *custom_fs_mount(struct file_system_type *fs_type,
-                                      int flags, const char *dev_name, void *data)
+static struct dentry *custom_fs_mount(struct file_system_type *fs_type, int flags, const char *dev_name, void *data)
 {
     struct dentry *entry;
     struct custom_fs_data *fs_data;
@@ -97,40 +139,64 @@ static struct dentry *custom_fs_mount(struct file_system_type *fs_type,
         return entry;
     }
 
-    // // Allocate memory for custom file system data
-    // fs_data = kzalloc(sizeof(struct custom_fs_data), GFP_KERNEL);
-    // if (!fs_data) {
-    //     pr_err("Failed to allocate memory for custom_fs_data\n");
-    //     goto out_fail;
-    // }
+    // Allocate memory for custom file system data
+    fs_data = kzalloc(sizeof(struct custom_fs_data), GFP_KERNEL);
+    if (!fs_data) {
+        pr_err("Failed to allocate memory for custom_fs_data\n");
+        goto out_fail;
+    }
 
-    // fs_data->sb = entry->d_sb->s_fs_info;
-    // entry->d_sb->s_fs_info = fs_data;
+    fs_data->sb = entry->d_sb->s_fs_info;
+    entry->d_sb->s_fs_info = fs_data;
 
-    // Create the "hello.txt" file
-    // file_name.name = "hello.txt";
-    // file_name.len = strlen(file_name.name);
-    
-    // file_dentry = d_alloc(entry, &file_name);
-    // if (!file_dentry) {
-    //     pr_err("Failed to allocate dentry for hello.txt\n");
-    //     goto out_fail;
-    // }
+    // Create the "/calc" directory
+    file_name.name = "calc";
+    file_name.len = strlen(file_name.name);
 
-    // file_inode = custom_fs_get_inode(entry->d_sb, S_IFREG | 0644);
-    // if (!file_inode) {
-    //     pr_err("Failed to allocate inode for hello.txt\n");
-    //     goto out_fail;
-    // }
+    file_dentry = d_alloc(entry, &file_name);
+    if (!file_dentry) {
+        pr_err("Failed to allocate dentry for /calc\n");
+        goto out_fail;
+    }
 
-    // file_dentry->d_inode = file_inode;
-    // file_dentry->d_op = &custom_fs_dentry_operations;
-    // file_dentry->d_sb = entry->d_sb;
+    file_inode = custom_fs_get_inode(entry->d_sb, S_IFDIR | 0755);
+    if (!file_inode) {
+        pr_err("Failed to allocate inode for /calc\n");
+        goto out_fail;
+    }
 
-    // inc_nlink(file_inode);
-    // d_instantiate(file_dentry, file_inode);
+    file_dentry->d_inode = file_inode;
+    file_dentry->d_op = &custom_fs_dentry_operations;
+    file_dentry->d_sb = entry->d_sb;
 
-    // return entry;
+    entry->d_sb->s_root = file_dentry;
+
+    // Create the "/calc/fib.num" file
+    file_name.name = "fib.num";
+    file_name.len = strlen(file_name.name);
+
+    file_dentry = d_alloc(file_dentry, &file_name);
+    if (!file_dentry) {
+        pr_err("Failed to allocate dentry for /calc/fib.num\n");
+        goto out_fail;
+    }
+
+    file_inode = custom_fs_get_inode(entry->d_sb, S_IFREG | 0644);
+    if (!file_inode) {
+        pr_err("Failed to allocate inode for /calc/fib.num\n");
+        goto out_fail;
+    }
+
+    file_dentry->d_inode = file_inode;
+    file_dentry->d_op = &custom_fs_dentry_operations;
+    file_dentry->d_sb = entry->d_sb;
+
+    file_inode->i_fop = &custom_fs_file_operations;
+
+    inc_nlink(file_inode);
+    d_instantiate(file_dentry, file_inode);
+
+    return entry;
 
 out_fail:
     // Cleanup on failure
@@ -138,29 +204,15 @@ out_fail:
     return ERR_PTR(-ENOMEM);
 }
 
-
 static void custom_fs_kill_super(struct super_block *sb)
 {
+    struct custom_fs_data *fs_data = sb->s_fs_info;
+
     kill_block_super(sb);
 
-    pr_info("unmounted disk\n");
-    // struct inode *root_inode = d_inode(sb->s_root);
+    pr_info("Unmounted disk\n");
 
-    // // Clean up resources associated with the root inode
-    // if (root_inode) {
-    //     // Cleanup operations specific to your file system's root inode
-    //     // ...
-
-    //     // Release the root inode
-    //     iput(root_inode);
-    // }
-    // // iput(root_inode);
-
-    // // Perform any additional cleanup operations specific to your file system
-    // // ...
-
-    // // Free the superblock structure
-    // kfree(sb);
+    kfree(fs_data);
 }
 
 // Custom file system file system type
@@ -169,7 +221,6 @@ static struct file_system_type custom_fs_type = {
     .mount = custom_fs_mount,
     .kill_sb = custom_fs_kill_super,
 };
-
 
 // Initialize the custom file system module
 static int __init custom_fs_init(void)
@@ -194,7 +245,6 @@ static void __exit custom_fs_exit(void)
 
     pr_info("Custom file system module unloaded\n");
 }
-
 
 module_init(custom_fs_init);
 module_exit(custom_fs_exit);
