1f996217fb7821b51f9ae9487554ae0c007ac6f9
Haim Kasel <haim.kasel@compiralabs.com>
2023-05-22 13:24:48 +0300

    modify simple_vfs

--- simple_vfs.c
+++ simple_vfs.c
@@ -228,6 +228,14 @@ static int fs_fill_super(struct super_block *sb, void *data, int silent)
     struct inode *root_inode;
     struct fs_data *fs_data;
 
+    // Allocate memory for the superblock
+    sb = alloc_super();
+
+    if (!sb) {
+        // Handle memory allocation failure
+        return -ENOMEM;
+    }
+
     printk("sb start");
     sb->s_magic = FILE_SYSTEM_MAGIC;
     printk("sb->s_magic finish");
